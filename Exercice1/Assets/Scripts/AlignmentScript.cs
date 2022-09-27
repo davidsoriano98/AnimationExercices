e63caeb2b1437f797304077f0073ec962bd4b53e
David <david.soriano@enti.cat>
2022-09-27 10:29:39 +0200

    Make exercici 1 correct

--- Exercice1/Assets/Scripts/AlignmentScript.cs
+++ Exercice1/Assets/Scripts/AlignmentScript.cs
@@ -22,22 +22,42 @@ public class AlignmentScript : MonoBehaviour
         {
             case 1:
             {
-                    //Buscamos un vector perpendicular a los dos vectores. Será nuestro axis
+                    //Find the offset angles between target1 and tracker.
+                    //Use an “angle axis approach” to find explicitly the angle offsets,
+                    //then rotate using Rotate method. 
+
+
+                    //Take care of the X rotation
+
+                    //Search a perpendicular vector between the two vectors. It will be our axis
                     Vector3 axisX = Vector3.Cross(transform.right, target1.transform.right).normalized;
 
-                    //Buscamos un angulo de rotacion -> El angulo tiene que ser negativo
+                    //Find the angle to rotate -> The angle needs to be negative.
                     float angleX = -Mathf.Acos(Vector3.Dot(transform.right, target1.transform.right)) * Mathf.Rad2Deg;
+                    
+                    //Make sure the angle is not an "imposible" angle. Near to 0 appear lots of vectors and the rotation explotes 
+                    if(Mathf.Abs(angleX) > 0.01)
+                    {
+                        //Apply the X rotation in world space.
+                        target1.transform.Rotate(axisX, angleX, Space.World);
+                    }
 
-                    //Aplicamos la rotacion al objeto 1 en world space
-                    target1.transform.Rotate(axisX, angleX,Space.World);
 
-                    //Same for Y axis
+
+                    //Take care of the Y rotation
+
+                    //Search a perpendicular vector between the two vectors. It will be our axis
                     Vector3 axisY = Vector3.Cross(transform.up, target1.transform.up).normalized;
 
-                    //El angulo tiene que ser negativo
+                    //Find the angle to rotate -> The angle needs to be negative.
                     float angleY = -Mathf.Acos(Vector3.Dot(transform.up, target1.transform.up)) * Mathf.Rad2Deg;
 
-                    target1.transform.Rotate(axisY, angleY,Space.World);
+                    //Make sure the angle is not an "imposible" angle. Near to 0 appear lots of vectors and the rotation explotes 
+                    if (Mathf.Abs(angleY) > 0.01)
+                    {
+                        //Apply the Y rotation in world space.
+                        target1.transform.Rotate(axisY, angleY, Space.World);
+                    }
 
                 } break;
 
